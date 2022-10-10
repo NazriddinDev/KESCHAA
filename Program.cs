92c89b2bebb8bbcc933dc5658bb29ac846f18f31
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2022-10-10 16:48:25 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+string greeting = $"Hello, {name}";
+System.Console.WriteLine(greeting);
+System.Console.WriteLine("Entr your age: ");
+string ageAsString =Console.ReadLine();
+System.Console.WriteLine("Converting...");
+int age =Convert.ToInt32(ageAsString);
+System.Console.WriteLine($"Successfully converted ! {age}");
+
+
+int keschaAge=5;
+int ageDifference = age - keschaAge;
+System.Console.WriteLine($"The difference between your and Kescha's age is {ageDifference}");
