24e51552fd24ea228929800dad40a1ca8624018b
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2022-10-13 15:21:28 +0500

    Clean

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 ﻿using KESCHAA.Classes;
 
-Animal keschaa = new Animal(name: "Kescha", age: 2);
+namespace KESCHAA
+{
+    class Program
+    {
+        static void Main()
+        {
+            Animal keschaa = new Animal(name: "Kescha", age: 2);
 
-Console.WriteLine("Enter your name: ");
-string userName = Console.ReadLine();
-keschaa.Greet(userName);
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            keschaa.Greet(userName);
 
-System.Console.WriteLine("Entr your age: ");
-string ageAsString =Console.ReadLine();
-System.Console.WriteLine("Converting...");
-int userAge =Convert.ToInt32(ageAsString);
-System.Console.WriteLine($"Successfully converted ! {userAge}");
+            System.Console.WriteLine("Entr your age: ");
+            string ageAsString =Console.ReadLine();
+            System.Console.WriteLine("Converting...");
+            int userAge =Convert.ToInt32(ageAsString);
+            System.Console.WriteLine($"Successfully converted ! {userAge}");
 
 
-keschaa.CalculateAgeDifferenceWithNoReturn(userAge);
-keschaa.PrintAgeDifferent();
-keschaa.CompareAges(userAge);
-keschaa.TellAboutFriends(userName, userAge);
+            keschaa.CalculateAgeDifferenceWithNoReturn(userAge);
+            keschaa.PrintAgeDifferent();
+            keschaa.CompareAges(userAge);
+            keschaa.TellAboutFriends(userName, userAge);
+        }
+    }
+}
+
